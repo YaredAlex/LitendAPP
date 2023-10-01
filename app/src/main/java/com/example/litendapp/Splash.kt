e878b2c0195bfba5521e1df767522b8da22bade5
YaredAlex <yareda25@gmail.com>
2023-10-01 17:02:10 +0530

    adding Splash Screen

---
+++ app/src/main/java/com/example/litendapp/Splash.kt
@@ -0,0 +1,18 @@
+package com.example.litendapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            var intent = Intent(this@Splash,MainActivity::class.java);
+             startActivity(intent);
+             finish();
+        },2000)
+    }
+}
