0274315ebd4129c67706bc411abf647390ba3ef2
YaredAlex <yareda25@gmail.com>
2023-10-06 22:32:17 +0530

    changes

--- app/src/main/java/com/example/litendapp/ui/login/LoginFormState.kt
+++ app/src/main/java/com/example/litendapp/ui/login/LoginFormState.kt
@@ -3,7 +3,7 @@ package com.example.litendapp.ui.login
 /**
  * Data validation state of the login form.
  */
-data class oginFormState(
+data class LoginFormState(
     val usernameError: Int? = null,
     val passwordError: Int? = null,
     val isDataValid: Boolean = false
