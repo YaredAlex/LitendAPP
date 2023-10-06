a77c6d2cadd4103a036b4a05e30fc7742c3eeffe
YaredAlex <yareda25@gmail.com>
2023-10-06 22:30:48 +0530

    adding user auth and routing

--- app/src/main/java/com/example/litendapp/MainActivity.kt
+++ app/src/main/java/com/example/litendapp/MainActivity.kt
@@ -4,14 +4,30 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.Toast
+import com.example.litendapp.databinding.ActivityMainBinding
 import com.example.litendapp.ui.login.LoginActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+//        binding.btnMenu.setOnClickListener{
+//            Toast.makeText(this@MainActivity,"clicked in main",Toast.LENGTH_LONG).show();
+//        }
+        setContentView(binding.root)
 
-        var intent = Intent(this@MainActivity,LoginActivity::class.java);
-        startActivity(intent);
+        var auth = Firebase.auth;
+        if(auth.currentUser==null){
+            var intent = Intent(this@MainActivity,LoginActivity::class.java);
+            startActivity(intent);
+        }
+        else{
+
+            Toast.makeText(this@MainActivity,"Logged in User",Toast.LENGTH_LONG).show()
+        }
+
     }
 }
