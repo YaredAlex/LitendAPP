a36c40bdb40078bf83537d3b554c9323d8cf2505
YaredAlex <yareda25@gmail.com>
2023-10-06 22:28:59 +0530

    creating register model

---
+++ app/src/main/java/com/example/litendapp/data/model/RegisterUserModel.kt
@@ -0,0 +1,8 @@
+package com.example.litendapp.data.model
+
+class RegisterUserModel (
+    var userName:String="",
+    var email:String="",
+    var password:String="",
+    var phoneNumber:String="",
+)
